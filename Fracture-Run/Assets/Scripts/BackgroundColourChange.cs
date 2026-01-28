247a4ec8d328b4f0ca7418ad8b36da605f59e2f5
sairamyedida06 <sairamyedidaoffl@gmail.com>
2026-01-28 20:34:22 +0530

    Add Dynamic environment

--- Fracture-Run/Assets/Scripts/BackgroundColourChange.cs
+++ Fracture-Run/Assets/Scripts/BackgroundColourChange.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class BackgroundColourChange : MonoBehaviour
 {
-    public Color[] colours;
+    
+    public Material[] skyboxes;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(ColourChange());
+        StartCoroutine(SkyboxChange());
     }
 
     // Update is called once per frame
@@ -17,18 +19,18 @@ public class BackgroundColourChange : MonoBehaviour
         
     }
 
-    IEnumerator ColourChange()
+    IEnumerator SkyboxChange()
     {
         while (true)
         {
-             int random = Random.Range(0,11);
+            int random = Random.Range(0, skyboxes.Length);
 
-            Camera.main.backgroundColor = colours[random];
+            RenderSettings.skybox = skyboxes[random];
+            DynamicGI.UpdateEnvironment(); 
 
             yield return new WaitForSeconds(8f);
-
-
         }
     }
+
 
 }
